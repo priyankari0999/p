7ec3b9a29d6e39fe5400bf8c8d566a28b5a584e5
Priyanka T <priyankat@Priyankas-MacBook-Pro.local>
2019-07-12 10:22:26 -0700

    code for panning Home

--- javascrpit.js
+++ javascrpit.js
@@ -1,1 +1,34 @@
 alert("WELCOME")
+var view;
+var map;
+var initialLocations;
+var london;
+
+function init() {
+  initialLocations = ol.proj.fromLonHat([41.043316, 28.862457])
+  london = ol.proj.fromLonLat([-0.12755,51.507222]);
+
+  view = new ol.view({
+    center: initialLocations,
+    zoom :6
+  map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source:new ol.source.OSM()
+      })
+ ],
+  loadTilesWhileAnimating : true,
+  view: view
+
+});
+}
+function panHome(){
+  view.animate({
+    center : london ,
+    duration: 2000
+
+  });
+}
+
+window.onload = init;
